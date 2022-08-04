10e3af16d437353a480737c7850037e6e8fd4b80
Ahmad CHAMMA <52955073+achamma723@users.noreply.github.com>
2022-08-04 04:31:43 +0200

    Update VarImp.R

--- R/VarImp.R
+++ R/VarImp.R
@@ -12,12 +12,12 @@ as.VarImp.data.frame <- function(object, FUN = mean,
    match.fun(FUN)
    # Add p-values part
    # perror (p x nperm)
-   perror_mean <- apply(object, 2, mean)
-   perror_std <- apply(object, 2, sd)
+   perror_mean <- apply(object, 1, mean)
+   perror_std <- apply(object, 1, sd)
    z_test = perror_mean / perror_std
    p_val = 1 - stats::pnorm(z_test)
 
-   out <- list(values = apply(object, 2, FUN, ...),
+   out <- list(values = apply(object, 1, FUN, ...),
                p_val = p_val,
                perTree = object,
                type = match.arg(type),
